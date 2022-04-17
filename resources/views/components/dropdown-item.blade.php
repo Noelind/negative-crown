36b3c0a0b963ae04f08c3671feec3c16b5e64c84
Panagiotis Penloglou <panagiotis.penloglou@protonmail.com>
2022-04-17 19:45:29 +0300

    Test Commint on New Dev Branch

--- resources/views/components/dropdown-item.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['active' => false])
-
-@php
-    $classes = "block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white";
-    if ($active) $classes .= ' bg-blue-300 text-white'; 
-    // Leave whitespace character when appending classes to not conflict with previous declarations
-@endphp 
-
-<a {{ $attributes(['class' => $classes]) }}> {{$slot}}</a>
